ebbb70ab62faf39095778310419a9c40787a9248
alxferraz <andrelxferraz@gmail.com>
2013-05-31 17:28:17 -0300

    Arquivo que recebe e processa os dados de novos cadastros

---
+++ PartyPay-PHP/controller/processaCadastro.php
@@ -0,0 +1,38 @@
+<?php
+
+include '../model/Pessoa.php';
+include 'gerarCodConfirmacao.php';
+
+$primeiroNome= addslashes($_POST['primeironome']);
+$sobreNome=  addslashes($_POST['sobreNome']);
+$email=  addslashes($_POST['email']);
+$password=  addslashes($_POST['password']);
+$sexo=  addslashes($_POST['sexo']);
+
+
+addcslashes($primeiroNome,$strList);
+addcslashes($sobreNome,$strList);
+addcslashes($email,$strList);
+addcslashes($password,$strList);
+addcslashes($sexo,$strList);
+
+
+
+$codigo=gerarCodigoConfirmaçao();
+$pessoa=new Pessoa();
+
+    $pessoa->setEmail($email);
+    $pessoa->setPrimeiroNome();
+    $pessoa->setSobreNome($sobreNome);
+    $pessoa->setPassword(md5($password));//criptografia md5 para o password
+    $pessoa->setSexo($sexo);
+
+
+$pessoa->setCodConfirmacao($codigo);
+$pessoa->persist();
+
+header("Location:../controller/EnviarEmailConfirmacao.php?e=".$_POST['email']."&cod=".$codigo);
+
+    
+
+?>
