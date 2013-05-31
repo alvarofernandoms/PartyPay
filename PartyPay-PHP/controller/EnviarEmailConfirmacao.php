c1cef3b1563eaf1e6c96e060c17e13c30e1f5382
Geisonszo <geisonszo@gmail.com>
2013-05-31 17:32:38 -0300

    Arquivo que manda mensagem de confirmacao para o usuario

---
+++ PartyPay-PHP/controller/EnviarEmailConfirmacao.php
@@ -0,0 +1,34 @@
+
+<?php
+include 'gerarCodConfirmacao.php';
+// multiple recipients
+$to  = $_GET['e'] ;
+$cod= $_GET['cod'];
+
+// subject
+$subject = 'PartyPay - Confirmação de Usuário';
+
+// message
+$message = '
+<html>
+<head>
+  <title>Confirme seu cadastro no PartyPay!</title>
+</head>
+<body>
+  <p>Clique <a href="http://www.partypay.webuda.com/controller/verify.php?op='.$cod.'&e='.$to.'">aqui</a> para confirmar seu cadastro</p>
+ 
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'From: PartyPay <no-reply@partypay.com>' . "\r\n";
+
+// Mail it
+mail($to, $subject, $message, $headers);
+
+?>
