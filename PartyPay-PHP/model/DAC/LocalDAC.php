d4b157bfe60a5cb321db50aba2b3752eead29e74
Álvaro Fernando <alvarofernandoms@hotmail.com>
2013-06-01 14:39:52 -0300

    Ajustando a camada LocalDAC.php

--- PartyPay-PHP/model/DAC/LocalDAC.php
+++ PartyPay-PHP/model/DAC/LocalDAC.php
@@ -2,21 +2,21 @@
 
 class LocalDAC {
 
-    public static function persist($evento) {
+    public static function persist($local) {
         include_once 'conexao.php';
         $sql = "INSERT INTO `locais`(`id`, `endereco`, `coordenadasGoogleMaps`, 
             `nome`, `numero`, `complemento`, `bairro`, `cidade`, `cep`, `pais`, 
             `fotos`, `estado`) 
             VALUES (NULL," . $local->getEndereco() . ","
-                . $local->getCoordenadasGoogleMaps() . "," 
-                . $local->getNome() . "," 
-                . $local->getNumero() . "," 
-                . $local->getComplemento() . "," 
-                . $local->getBairro() . "," 
-                . $local->getCidade() . "," 
-                . $local->getCep() . "," 
-                . $local->getPais() . "," 
-                . $local->getFotos() . "," 
+                . $local->getCoordenadasGoogleMaps() . ","
+                . $local->getNome() . ","
+                . $local->getNumero() . ","
+                . $local->getComplemento() . ","
+                . $local->getBairro() . ","
+                . $local->getCidade() . ","
+                . $local->getCep() . ","
+                . $local->getPais() . ","
+                . $local->getFotos() . ","
                 . $local->getEstado() . ")";
         mysql_query($sql) or die(mysql_error());
         return mysql_insert_id();
