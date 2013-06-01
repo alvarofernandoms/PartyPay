07d02e4ab361b76839280c18f50c03f5e690dcc7
Geisonszo <geisonszo@gmail.com>
2013-06-01 11:09:53 -0300

    Camada de persistencia do evento

---
+++ PartyPay-PHP/model/DAC/EventoDAC.php
@@ -0,0 +1,43 @@
+<?php
+
+class EventoDAC {
+    
+    public static function persist($evento){
+        include_once 'conexao.php';
+        $sql="INSERT INTO `eventos`(`nome`, 
+            `dataInicio`, `dataTermino`, `imagem`,
+            `precoMasc`, `precoFem`, `organizadorID`,
+            `localID`, `facebookEventPage`, `id`,
+            `dataCriacao`, `descricao`) VALUES (";
+        $sql.=$evento->getNome().",";
+        $sql.=$evento->getDataInicio().",";
+        $sql.=$evento->getDataTermino().",";
+        $sql.=$evento->getImage().",";
+        $sql.=$evento->getPrecoMasc().",";
+        $sql.=$evento->getPrecoFem().",";
+        $sql.=$evento->getOrganizador()->getId().",";
+        $sql.=$evento->getLocal()->getId().",";
+        $sql.=$evento->getFacebookEventPage().", NULL,";
+        $sql.=$evento->getDataCriacao().",";
+        $sql.=$evento->getDescricao().")";
+        
+        mysql_query($sql) or die(mysql_error());
+        mysql_close($cn);
+    }
+    
+     public static function updateInfo($evento,$atributo,$atributoNovo){
+         include_once 'conexao.php';
+         $sql="UPDATE `eventos` SET `$atributo`=$atributoNovo WHERE id=".$evento->getId();
+        mysql_query($sql)or die(mysql_error());
+        mysql_close($cn);
+    }
+    
+    public static function delete($evento){
+        include_once 'conexao.php'; 
+        $sql="DELETE FROM `eventos` WHERE id=".$evento->getId();
+        mysql_query($sql)or die(mysql_error());
+        mysql_close($cn);
+    }
+}
+
+?>
