1f0489a55f205e4d09c0ef258a5061332a9716e6
Álvaro Fernando <alvarofernandoms@hotmail.com>
2013-06-01 14:03:05 -0300

    Modificação da função persist na camada LocalDAC.php  e alterações mínimas em Local.php

--- PartyPay-PHP/model/DAC/LocalDAC.php
+++ PartyPay-PHP/model/DAC/LocalDAC.php
@@ -1,32 +1,42 @@
 <?php
 
 class LocalDAC {
-     
-    public static function persist($evento){
-       include_once 'conexao.php';
-        $sql="INSERT INTO `locais`(`id`, `endereco`, `coordenadasGoogleMaps`, `nome`) VALUES (NULL,".$evento->getEndereco().",".$evento->getCoordenadasGoogleMaps().",".$evento->getNome().")";
+
+    public static function persist($evento) {
+        include_once 'conexao.php';
+        $sql = "INSERT INTO `locais`(`id`, `endereco`, `coordenadasGoogleMaps`, 
+            `nome`, `numero`, `complemento`, `bairro`, `cidade`, `cep`, `pais`, 
+            `fotos`, `estado`) 
+            VALUES (NULL," . $local->getEndereco() . ","
+                . $local->getCoordenadasGoogleMaps() . "," 
+                . $local->getNome() . "," 
+                . $local->getNumero() . "," 
+                . $local->getComplemento() . "," 
+                . $local->getBairro() . "," 
+                . $local->getCidade() . "," 
+                . $local->getCep() . "," 
+                . $local->getPais() . "," 
+                . $local->getFotos() . "," 
+                . $local->getEstado() . ")";
         mysql_query($sql) or die(mysql_error());
         return mysql_insert_id();
         mysql_close($cn);
-        
     }
-    
-     public static function updateInfo($local,$atributo,$atributoNovo){
+
+    public static function updateInfo($local, $atributo, $atributoNovo) {
         include_once 'conexao.php';
-         $sql="UPDATE `locais` SET `$atributo`=$atributoNovo WHERE id=".$local->getId();
-        mysql_query($sql)or die(mysql_error());
-        mysql_close($cn);
-        
-    }
-    
-    public static function delete($local){
-        include_once 'conexao.php'; 
-        $sql="DELETE FROM `locais` WHERE id=".$local->getId();
-        mysql_query($sql)or die(mysql_error());
+        $sql = "UPDATE `locais` SET `$atributo`=$atributoNovo WHERE id=" . $local->getId();
+        mysql_query($sql) or die(mysql_error());
         mysql_close($cn);
     }
+
+    public static function delete($local) {
+        include_once 'conexao.php';
+        $sql = "DELETE FROM `locais` WHERE id=" . $local->getId();
+        mysql_query($sql) or die(mysql_error());
+        mysql_close($cn);
+    }
+
 }
-
-
 
 ?>
--- PartyPay-PHP/model/Local.php
+++ PartyPay-PHP/model/Local.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once 'DAC/LocalDAC.php';
 class Local {
