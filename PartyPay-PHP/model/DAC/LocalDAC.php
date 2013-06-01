f9e994ff74f38c0209e1be20b0567c78b3ac70d9
fanig01 <fanig_01@hotmail.com>
2013-06-01 11:56:11 -0300

    Atualizacao de Local DAC

--- PartyPay-PHP/model/DAC/LocalDAC.php
+++ PartyPay-PHP/model/DAC/LocalDAC.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of LocalDAC
- *
- * @author Andre
- */
 class LocalDAC {
      
     public static function persist($evento){
