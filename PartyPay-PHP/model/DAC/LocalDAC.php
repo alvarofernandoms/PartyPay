8bceadec3fa01c4d694716adc346770c13f8349e
fanig01 <fanig_01@hotmail.com>
2013-06-01 11:49:20 -0300

    Camada de persistencia do Local php

---
+++ PartyPay-PHP/model/DAC/LocalDAC.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of LocalDAC
+ *
+ * @author Andre
+ */
+class LocalDAC {
+     
+    public static function persist($evento){
+       include_once 'conexao.php';
+        $sql="INSERT INTO `locais`(`id`, `endereco`, `coordenadasGoogleMaps`, `nome`) VALUES (NULL,".$evento->getEndereco().",".$evento->getCoordenadasGoogleMaps().",".$evento->getNome().")";
+        mysql_query($sql) or die(mysql_error()); 
+        mysql_close($cn);
+    }
+    
+     public static function updateInfo($local,$atributo,$atributoNovo){
+        include_once 'conexao.php';
+         $sql="UPDATE `locais` SET `$atributo`=$atributoNovo WHERE id=".$local->getId();
+        mysql_query($sql)or die(mysql_error());
+        mysql_close($cn);
+        
+    }
+    
+    public static function delete($local){
+        include_once 'conexao.php'; 
+        $sql="DELETE FROM `locais` WHERE id=".$local->getId();
+        mysql_query($sql)or die(mysql_error());
+        mysql_close($cn);
+    }
+}
+
+
+
+?>
