e2b131f1a3e163b158403c2f9ab63e431f0b911b
alxferraz <andrelxferraz@gmail.com>
2013-06-01 14:44:32 -0300

    [André Ferraz]:Processa Cadastro de Eventos

--- PartyPay-PHP/controller/processaCadastroEvento.php
+++ PartyPay-PHP/controller/processaCadastroEvento.php
@@ -1,5 +1,34 @@
 <?php
 
-$localID=$_SESSION['localid'];
+$nome=$_POST['nome'];
+$dataInicio=$_POST['dataInicio'];
+$dataTermino=$_POST['dataTermino'];
+$preçoMasc=$_POST['precoMasc'];
+$precoFem=$_POST['precoFem'];
+$local=$_SESSION['localid'];
+$facebookEventPage=$_POST['facebookEventPage'];
+$descricao=$_POST['descricao'];
+$numeroDeIngressos=$_POST['numeroDeIngressos'];
+$horaInicio=$_POST['horaInicio'];
+$horaTermino=$_POST['horaTermino'];
+
+$evento= new Evento();
+
+$evento->setNome($nome);
+$evento->setDataInicio($dataInicio);
+$evento->setDataTermino($dataTermino);
+$evento->setHoraInicio($horaInicio);
+$evento->setHoraTermino($horaTermino);
+$evento->setNumeroIngressos($numeroDeIngressos);
+$evento->setOrganizador($_SESSION['user_id']);
+$evento->setPrecoFem($precoFem);
+$evento->setPreçoMasc($preçoMasc);
+$evento->setFacebookEventPage($facebookEventPage);
+$evento->setLocal($local);
+$evento->setDescricao($descricao);
+
+
+
+
 
 ?>
