ecef2ccefcd4ec5eb9d6e7a4ba4c5d465215af2e
alxferraz <andrelxferraz@gmail.com>
2013-06-01 12:27:21 -0300

    [André Ferraz]:Inclusao Processa Cadastro Evento

--- PartyPay-PHP/model/DAC/LocalDAC.php
+++ PartyPay-PHP/model/DAC/LocalDAC.php
@@ -5,8 +5,10 @@ class LocalDAC {
     public static function persist($evento){
        include_once 'conexao.php';
         $sql="INSERT INTO `locais`(`id`, `endereco`, `coordenadasGoogleMaps`, `nome`) VALUES (NULL,".$evento->getEndereco().",".$evento->getCoordenadasGoogleMaps().",".$evento->getNome().")";
-        mysql_query($sql) or die(mysql_error()); 
+        mysql_query($sql) or die(mysql_error());
+        return mysql_insert_id();
         mysql_close($cn);
+        
     }
     
      public static function updateInfo($local,$atributo,$atributoNovo){
