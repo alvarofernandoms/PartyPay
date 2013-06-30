03e376e9d6c029682be583a8dffeb23015278aae
Geisonszo <geisonszo@gmail.com>
2013-06-30 16:18:56 -0300

    header e footer na view

--- PartyPay-PHP/view/cadastrar-evento-local.php
+++ PartyPay-PHP/view/cadastrar-evento-local.php
@@ -1,26 +1,4 @@
-<html>
-  <head>
-    <title>Party Pay</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="view/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="view/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-    <link href="view/css/style.css" rel="stylesheet" media="screen">
-  </head>ink href="view/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-  </head>
-  <body>
-		<header>
-			<div class="navbar">
-			  <div class="navbar-inner">
-			    <a class="brand" href="#"><img src="view/img/marca-mini.png" alt="" /></a>
-			    <ul class="nav">
-			      <li class="active"><a href="index.php">Home</a></li>
-			      <li><a href="#">Sobre</a></li>
-			      <li><a href="#">Eventos</a></li>
-			    </ul>
-			  </div>
-			</div>
-		</header>
+<?php require_once ('header.php'); ?>
 		<section class="container">
     		<h3>Local</h3>
 			<div class="row">
@@ -93,12 +71,4 @@
 				</form>
     		</div>
 		</section>
-		<footer>
-		</footer>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="view/js/bootstrap.min.js"></script>
-    <script src="view/js/bootstrap-responsive.min.js"></script>
-    <script src="view/js/jquery.maskedinput.min.js"></script>
-
-  </body>
-</html>
+		<?php require_once ('footer.php'); ?>
--- PartyPay-PHP/view/cadastrarEvento.php
+++ PartyPay-PHP/view/cadastrarEvento.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Party Pay</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap -->
-        <link href="view/css/bootstrap.min.css" rel="stylesheet" media="screen">
-        <link href="view/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-        <link href="view/css/style.css" rel="stylesheet" media="screen">
-    </head>
-    <body>
-        <header>
-            <div class="navbar">
-                <div class="navbar-inner">
-                    <a class="brand" href="index.php"><img src="view/img/marca-mini.png" alt="" /></a>
-                    <ul class="nav">
-                        <li class="active"><a href="index.php">Home</a></li>
-                        <li><a href="#">Sobre</a></li>
-                        <li><a href="cadastrarPessoa.php">Sign up</a></li>
-                    </ul>
-                    <a class="btn btn-success" href="cadastrarEvento.php">Divulgar seu evento</a>
-                </div>
-            </div>
-        </header>
+<?php require_once('header.php'); ?>
         <section class="container">
             <h3>Novo Evento</h3>
             <div class="row">
@@ -288,12 +265,4 @@
                                 </form>
                         </div>
                         </section>
-                        <footer>
-                        </footer>
-                        <script src="http://code.jquery.com/jquery.js"></script>
-                        <script src="view/js/bootstrap.min.js"></script>
-                        <script src="view/js/bootstrap-responsive.min.js"></script>
-                        <script src="view/js/jquery.maskedinput.min.js"></script>
-
-                        </body>
-                        </html>
+                        <?php require_once('footer.php'); ?>
