42cbda63795c3f84596a3939b236e8e9b324be75
leandrokanis <leandrosustenido@gmail.com>
2013-07-03 15:23:04 -0300

    Teste da classe Pessoa - gets e sets

--- PartyPay-PHP/tests/test-model/PessoaTEST.php
+++ PartyPay-PHP/tests/test-model/PessoaTEST.php
@@ -7,13 +7,51 @@ class PessoaTEST extends PHPUnit_Framework_TestCase{
 
     public function setUp(){
     	$this->test = new Pessoa();
-    	$primeiroNome = $this->test->setPrimeiroNome("leandro");
     }
 
-    public function testPrimeiroNome(){
-    	$leandro = $this->test->getPrimeiroNome();
-    	$this->assertTrue($leandro == "leandro");
+    public function testInsercaoPrimeiroNome(){
+    	$primeiroNome = $this->test->setPrimeiroNome("nome");
+    	$nome = $this->test->getPrimeiroNome();    	
+    	$this->assertTrue($nome == "nome");
     }
+    public function testInsercaoSobreNome(){
+    	$sobreNome = $this->test->setSobreNome("sobrenome");
+    	$nome = $this->test->getSobreNome();    	
+    //	$this->assertTrue($nome == "sobrenome");
+    	$this->assertEquals("sobrenome", $nome);
+    }
+
+    public function testInsercaoEmail(){
+    	$email = $this->test->setEmail("email");
+    	$nome = $this->test->getEmail();    	
+    //	$this->assertTrue($nome == "email");
+    	$this->assertEquals("email", $nome);
+    }
+
+    public function testInsercaoId(){
+    	$Id = $this->test->setId("Id");
+    	$nome = $this->test->getId();    	
+    //	$this->assertTrue($nome == "Id");
+    	$this->assertEquals("Id", $nome);
+    }
+
+    public function testPassword(){
+    	$Password = $this->test->setPassword("Password");
+    	$nome = $this->test->getPassword();    	
+    //	$this->assertTrue($nome == "Password");
+    	$this->assertEquals("Password", $nome);
+    }
+    
+    public function testSexo(){
+    	$Sexo = $this->test->setId("Feminino");
+    	$nome = $this->test->getSexo();    	
+    //	$this->assertTrue($nome == "Sexo");
+    	$this->assertEquals("Masculino", $nome);
+    }
+
+    // TODO continuar daqui
+    // O último teste está falhando para marcar a continuação apartir daqui
+    // Fazer os demais parametros
 }
 
 ?>
