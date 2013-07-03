51a2d47c00b1beefb68ac583ecfd07facd137808
leandrokanis <leandrosustenido@gmail.com>
2013-07-03 00:18:49 -0300

    Teste de PrimeiroNome

--- PartyPay-PHP/tests/test-model/PessoaTEST.php
+++ PartyPay-PHP/tests/test-model/PessoaTEST.php
@@ -1,7 +1,19 @@
 <?php
 
+include '../../model/Pessoa.php';
+
 class PessoaTEST extends PHPUnit_Framework_TestCase{
-    
+    public $test;
+
+    public function setUp(){
+    	$this->test = new Pessoa();
+    	$primeiroNome = $this->test->setPrimeiroNome("leandro");
+    }
+
+    public function testPrimeiroNome(){
+    	$leandro = $this->test->getPrimeiroNome();
+    	$this->assertTrue($leandro == "leandro");
+    }
 }
 
 ?>
