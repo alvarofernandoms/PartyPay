1707e1bead1afbc04bab6e4582f4c821aa62ebcc
Geisonszo <geisonszo@gmail.com>
2013-07-04 00:33:11 -0300

    Classe que envia confirmacao de email

--- PartyPay-PHP/controller/EnviarEmailConfirmacao.php
+++ PartyPay-PHP/controller/EnviarEmailConfirmacao.php
@@ -1,34 +1,33 @@
-
 <?php
 include 'gerarCodConfirmacao.php';
 // multiple recipients
-$to  = $_GET['e'] ;
-$cod= $_GET['cod'];
+$to  = $_GET['email'] ;
+$cod = $_GET['cod'];
 
-// subject
+//Assunto
 $subject = 'PartyPay - Confirmação de Usuário';
 
-// message
+//Mensagem
 $message = '
 <html>
 <head>
   <title>Confirme seu cadastro no PartyPay!</title>
 </head>
 <body>
-  <p>Clique <a href="http://www.partypay.webuda.com/controller/verify.php?op='.$cod.'&e='.$to.'">aqui</a> para confirmar seu cadastro</p>
+  <p>Clique <a href="http://www.partypay.com/controller/verify.php?op='.$cod.'&email='.$to.'">aqui</a> para confirmar seu cadastro!</p>
  
 </body>
 </html>
 ';
 
-// To send HTML mail, the Content-type header must be set
+// header sendo definido para enviar Email HTML
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-// Additional headers
+//headers adicionais
 $headers .= 'From: PartyPay <no-reply@partypay.com>' . "\r\n";
 
-// Mail it
+// Mandando o Email
 mail($to, $subject, $message, $headers);
 
 ?>
