7a23c621a49379f8e5670fc049ee3d71b12bac72
Geisonszo <geisonszo@gmail.com>
2013-07-12 23:47:19 -0300

    Refatoracao EnviarEmailNovaSenha.php

--- PartyPay-PHP/controller/EnviarEmailNovaSenha.php
+++ PartyPay-PHP/controller/EnviarEmailNovaSenha.php
@@ -1,28 +1,23 @@
 <?php
    
-    $user_email = $_GET['email'];
+    $user_email = $_POST['email'];
     
-    $CaracteresAceitos = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $CaracteresAceitos = 'defghABC0123456789';
     $max = strlen($CaracteresAceitos)-1;
-    $novasenha = null;
+    $novasenha = '';
     
     for($i=0; $i < 8; $i++) {
         $novasenha .= md5($CaracteresAceitos{mt_rand(0, $max)});
     }
     
-    require_once 'conexao.php';
+    require_once '../model/DAC/conexao.php';
+    $sql =  "SELECT * FROM `pessoas` WHERE email= '$user_email' ";
+    $retorno = mysql_query($sql)or die(mysql_error());
     
-    $sql_consulta_email =  ("SELECT * FROM `pessoas` WHERE `email` like '$user_email.' ");
+    if(mysql_num_rows($retorno)===1){ 
     
-    $retorno = mysql_query($sql_consulta_email);
-    
-    if($retorno){
-        
-    $sql_update_senha = ("UPDATE `pessoas` SET `senha`= $novasenha WHERE `email` LIKE $user_email");
-            
-    $retorno_update = mysql_query($sql_update_senha);
-        
-        if($retorno_update){
+    $sql_update = "UPDATE `pessoas` SET `senha` = '$novasenha' WHERE `pessoas`.`email` ='$user_email';";     
+    mysql_query($sql_update)or die(mysql_error());
 
                 //Assunto
                   $subject = 'PartyPay - Nova senha';
@@ -30,20 +25,18 @@
                 //Mensagem
                     $message = "Sua <strong>nova</strong> senha: <strong>$novasenha</strong><br><br>";
     
-                //headers adicionais
-                $headers .= 'From: PartyPay <no-reply@partypay.com>' . "\r\n";
+                //headers adicionais agora vai
+                $headers = 'From: PartyPay <no-reply@partypay.com>' . "\r\n";
     
                 // Mandando o Email
                     mail($user_email, $subject, $message, $headers);
     
-                echo "Sua nova senha foi enviada para seu endereço de email!";
-        }
-    
-    }else{
-    
-    echo "Este Email não esta cadastrado!";
-    
+                echo "Um email foi enviado com sua nova senha!";
+        }else{
+                echo "Desculpe! Email inválido!";
+                
     }
+    mysql_query($sql) or die(mysql_error());
     mysql_close($conexao);
 
 ?>
