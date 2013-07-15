eb5a2064840192a8b551da300621dac504e61154
Álvaro Fernando <alvarofernandoms@hotmail.com>
2013-07-15 15:38:23 -0300

    Teste da função PERSIST relacionada com o cadastro de PESSOA

--- PartyPay-PHP/tests/test-controller/processaCadastroPessoaTEST.php
+++ PartyPay-PHP/tests/test-controller/processaCadastroPessoaTEST.php
@@ -1,19 +1,25 @@
 <?php
 
-include '../../controller/processaCadastroPessoa.php';
+include '../../model/Pessoa.php';
 
-class PessoaTEST extends PHPUnit_Framework_TestCase{
+class processaCadastroPessoaTEST extends PHPUnit_Framework_TestCase {
+
     public $test;
 
-    public function setUp(){
-    	$this->test = new Pessoa();
+    public function setUp() {
+        $this->test = new Pessoa();
     }
 
-    public function testInsercaoPrimeiroNome(){
-    	$primeiroNome = $_POST['primeiroNome'];
-        $this->test->setPrimeiroNome("nome");
-    	$nome = $this->test->getPrimeiroNome();    	
-    	$this->assertTrue($nome == "nome");
+    public function testFuncaoPersist() {
+        $this->test->setEmail("alvaro@gmail.com");
+        $this->test->setPrimeiroNome("teste 2 primeiro nome");
+        $this->test->setSobreNome("teste sobrenome");
+        $this->test->setPassword(md5("123456")); //criptografia md5 para o password
+        $this->test->setSexo("Masculino");
+        $this->test->setCpf("111.111.111-11");
+        $this->test->setTelefoneContato("(22) 2222-2222");
+        $resultadoTEST = $this->test->persist();
+        $this->assertTrue($resultadoTEST == !Null);
     }
 
 }
